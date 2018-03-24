f6f24c46c2815cd92728b301ce1e382103d18c8c
David Macro <david.macro@volvocars.com>
2018-03-24 23:07:51 +0100

    demo

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+VERSION = '1.0'
+
+@app.route('/')
+def index():
+    return 'version {}'.format(VERSION)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
