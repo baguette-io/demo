47bf87f5a5daf6e5e34c9db70cfa418f442c139a
David Macro <david.macro@volvocars.com>
2018-06-24 15:07:59 +0200

    init

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-VERSION = '5.53'
 
 @app.route('/')
 def index():
-    return str(VERSION)
+    return '1.0'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
